84bcd78bc3f4aa48bba5b7a8254daac97df57ceb
Fernando Aleman <fernandoaleman@mac.com>
2011-08-08 16:40:11 -0500

    Updated Redir URI again

--- shortlink_app.rb
+++ shortlink_app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 configure do
   require 'redis'
-  # uri = URI.parse(ENV["REDISTOGO_URL"])
+  uri = URI.parse(ENV["REDISTOGO_URL"])
   # REDIS = Redis.new
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
